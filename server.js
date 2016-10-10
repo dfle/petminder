2275460ba05ccec8d90a5918768c597f375a50c5
Dianne Le <diannefle@gmail.com>
2016-10-10 11:57:24 -0700

    Add dotenv, Configure twilio

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+require('dotenv').config();
+var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
 
 var app = express();
 var port = 9001;
@@ -7,4 +9,16 @@ app.use('/', express.static(__dirname + '/client'));
 
 app.listen(port, function() {
   console.log('connected to port: ', port);
+});
+
+client.sendMessage({
+  to: process.env.toNum,
+  from: process.env.fromNum,
+  body: 'testing'
+})
+.then(function(response) {
+  console.log('message sent', response);
+})
+.catch(function(err){
+  console.log('err: ', err);
 });
