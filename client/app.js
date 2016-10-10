db9bba3aac6f68518db2200141290ea266ff3d48
Dianne Le <diannefle@gmail.com>
2016-10-10 11:26:10 -0700

    Add styles, add Dog constructor, add dummy data

--- client/app.js
+++ client/app.js
@@ -1,6 +1,27 @@
 var app = angular.module('app', []);
 
-app.controller('AppController', function() {
+var data = ['Blanket', 'Willow'];
+
+app.controller('AppController', function(AppFactory) {
   var vm = this;
   vm.test = 'test';
+  vm.dogs = [];
+
+  data.forEach(function(dog) {
+    vm.dogs.push(new AppFactory.Dog(dog));
+  });
+});
+
+app.factory('AppFactory', function() {
+  var Dog = function(name) {
+    this.name = name;
+    this.morning = false;
+    this.noon = false;
+    this.evening = false;
+    this.night = false;
+  };
+
+  return {
+    Dog: Dog
+  }
 });
